9cd508706765d64256d25ba98b1e57da4d6324a1
denniskimotho <denniskimotho222@gmail.com>
2023-10-06 14:14:57 +0300

    Initial message

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const writeCards = function(gift,event){
+    let name=[];
+
+    for(let i = 0;i<gift.length;i++){
+        name.push(`Thank you, ${gift[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return name
+}
+
+const countDown = function(count){
+    while(count>=0){
+        console.log(count)
+        count --
+    }
+}
